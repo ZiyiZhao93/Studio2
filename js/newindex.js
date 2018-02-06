4331701a1fdc49ee54a4f6d41f44d0ebc4eb908c
ZiyiZhao93 <31828737+ZiyiZhao93@users.noreply.github.com>
2018-02-05 23:49:45 -0500

    2.5

--- js/newindex.js
+++ js/newindex.js
@@ -4,6 +4,9 @@ var height = document.getElementById('svg1').clientHeight;
 var width2 = document.getElementById('svg2').clientWidth;
 var height2 = document.getElementById('svg2').clientHeight;
 
+var width3 = document.getElementById('svg3').clientWidth;
+var height3 = document.getElementById('svg3').clientHeight;
+
 var marginLeft = 0;
 var marginTop = 0;
 
@@ -17,28 +20,30 @@ var svg2 = d3.select('#svg2')
     .append('g')
     .attr('transform', 'translate(' + marginLeft + ',' + marginTop + ')');
 
+var svg3 = d3.select('#svg3')
+    .append('g')
+    .attr('transform', 'translate(' + marginLeft + ',' + marginTop + ')');
+
 var albersProjection = d3.geoAlbers()
-    .scale(450000)
+    .scale(300000)
     .rotate( [71.057,0] )
     .center( [0, 42.313] )
     .translate([(width/3.7), (height/0.9)]);
 
-var albersProjection2 = d3.geoAlbers()
-    .scale(450000)
-    .rotate( [71.057,0] )
-    .center( [0, 42.313] )
-    .translate([(width2/3.7), (height2/0.75)]);
+var nestedData = [];
 
 path = d3.geoPath()
     .projection(albersProjection);
 
-path2 = d3.geoPath()
-    .projection(albersProjection2);
+var scaleX = d3.scaleBand().rangeRound([0, width3-2*marginLeft]).padding(0.05);
+var scaleY = d3.scaleLinear().range([height3-2*marginTop, 0]);
 
 queue()
-    .defer(d3.json, "eastboston.json")
-    .defer(d3.csv, "circle.csv")
-    .await(function (err, mapData, circleData) {
+    .defer(d3.json, "js/eastboston.json")
+    .defer(d3.json, "js/street.json")
+    .defer(d3.csv, "csv/eastboston-countrypopulation.csv")
+    .await(function (err, mapData, streetData, populationData) {
+
 
         svg.selectAll("path")
             .data(mapData.features)
@@ -46,24 +51,27 @@ queue()
             .append("path")
             .attr("d", path)
             .attr("class", "feature")
-            .attr('fill',"white");
-
+            .attr('fill', "white");
         svg2.selectAll("path")
-            .data(mapData.features)
+            .data(streetData.features)
             .enter()
             .append("path")
-            .attr("d", path2)
-            .attr("class", "feature")
-            .attr('fill',"white");
-
-        svg2.selectAll("circle")
-            .data(circleData)
-            .enter()
-            .append("circle")
-            .attr('cx', function (d) {return d.cx})
-            .attr('cy',function (d) {return d.cy})
-            .attr('r', function(d) {return d.r})
-            .attr('fill', function(d) {return d.fill});
+            .attr("d", path)
+            .attr("class", "street")
+            .attr("stroke", "lightgray")
+            .attr("stroke-width", 1)
+            .attr("fill", "none");
 
 
     });
+
+
+
+
+
+
+
+
+
+
+
