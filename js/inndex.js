07fd59979e65b2cebb893478287fb561722a098c
ZiyiZhao93 <31828737+ZiyiZhao93@users.noreply.github.com>
2018-02-13 02:43:51 -0500

    2.12

--- js/inndex.js
+++ js/inndex.js
@@ -4,8 +4,8 @@ var height = document.getElementById('svg1').clientHeight;
 var width2 = document.getElementById('svg2').clientWidth;
 var height2 = document.getElementById('svg2').clientHeight;
 
-var width3 = document.getElementById('svg3').clientWidth;
-var height3 = document.getElementById('svg3').clientHeight;
+// var width3 = document.getElementById('svg3').clientWidth;
+// var height3 = document.getElementById('svg3').clientHeight;
 
 var marginLeft = 0;
 var marginTop = 0;
@@ -25,7 +25,7 @@ var svg3 = d3.select('#svg3')
     .attr('transform', 'translate(' + marginLeft + ',' + marginTop + ')');
 
 var albersProjection = d3.geoAlbers()
-    .scale(300000)
+    .scale(400000)
     .rotate( [71.057,0] )
     .center( [0, 42.313] )
     .translate([(width/3.7), (height/0.9)]);
@@ -35,8 +35,8 @@ var nestedData = [];
 path = d3.geoPath()
     .projection(albersProjection);
 
-var scaleX = d3.scaleBand().rangeRound([0, width3-2*marginLeft]).padding(0.05);
-var scaleY = d3.scaleLinear().range([height3-2*marginTop, 0]);
+//var scaleX = d3.scaleBand().rangeRound([0, width3-2*marginLeft]).padding(0.05);
+//var scaleY = d3.scaleLinear().range([height3-2*marginTop, 0]);
 
 queue()
     .defer(d3.json, "js/eastboston.json")
